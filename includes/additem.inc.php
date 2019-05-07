434b880c2c7cfaf97fb71930f8fb369bf70b6f43
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-07 13:03:56 +0800

    Create additem.inc.php

---
+++ includes/additem.inc.php
@@ -0,0 +1,47 @@
+<?php 
+
+if(isset($_POST['additembtn'])){
+	require 'db.inc.php';
+
+	$code = "AGBI";
+	$companyname = $_POST['companyname'];
+	$brand = $_POST['productbrand'];
+	$code1 = $_POST['code1'];
+	$code2 = $_POST['code2'];
+	$producttype = $_POST['producttype'];
+	$stocks = $_POST['stocks'];
+		
+
+if(empty($companyname) || empty($brand) || empty($code1) || empty($code2) || empty($producttype) || empty($stocks)){
+	header("Location: ../additem.php?error=empty");
+	exit();
+}
+elseif($code1 !== $code2){
+	header("Location: ../additem.php?error=productcode");
+}
+else{
+			$sql = "INSERT INTO tblproducts (productID, productCompanyCode, productBrand, productModel, productDesc, productStocks) VALUES (?, ?, ?, ?, ?, ?)";
+			$stmt = mysqli_stmt_init($conn);
+
+			if(!mysqli_stmt_prepare($stmt, $sql)){
+				header("Location: ../additem.php?error=sqlerror");
+				exit();
+			}
+			else{
+				mysqli_stmt_bind_param($stmt, "ssssss",$code, $companyname, $brand, $code1, $producttype, $stocks);
+				mysqli_stmt_execute($stmt);
+				header("Location: ../additem.php?itemadded");
+				exit();
+			}
+		}
+	}	
+else{
+	header("Location: login.php");
+}
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+
+
+
+
+?>
