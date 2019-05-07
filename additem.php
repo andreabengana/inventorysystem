5aa2494549aea21e37989e3ce510a0d54eef3e2a
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-07 13:03:58 +0800

    Update additem.php

--- additem.php
+++ additem.php
@@ -15,39 +15,45 @@ else{
 <div class="container">
 			<div class="card-header text-center">
 				<a href="homepage.php" class="btn btn-danger float-left">Go Back</a><h1 style="color: black; margin-right: 90px;">Add Item</h1>
-						<form id="form1" action="includes/addemployee.inc.php" method="POST">
+						<form action="includes/additem.inc.php" method="POST">
 							<label for="row1"><h3> Item Details </h3></label>
   							<div class="form-row" id="row1" style="margin-top: 0px!important;">
     							<div class="form-group col-md-6">
       								<label for="inputfirstname">Company</label>
-      								<input type="text" class="form-control" id="inputfirstname" placeholder="Anderson Group BPO Inc." name="companyname">
+      								<input type="text" class="form-control" placeholder="Anderson Group BPO Inc." name="companyname">
     							</div>
     							<div class="form-group col-md-6">
      								 <label for="inputlastname">Brand</label>
-     								 <input type="text" class="form-control" id="inputlastname" placeholder="Acer" name="productbrand">
+     								 <input list="brands" class="form-control" placeholder="-Brand Name-" name="productbrand">
+                     <datalist id="brands">
+                        <option value="Acer">
+                        <option value="Samsung">
+                        <option value="Apple">
+                        <option value="Logitech">
+                     </datalist>
     							</div>
   							</div>
                 <div class="form-row" id="row2">
   								<div class="form-group col-md-6">
-  									<label for="inputemail">Model</label>
-  									<input type="text" name="email" id="inputmodel1" class="form-control" placeholder="-Product Code-">
+  									<label for="inputemail">Product Model</label>
+  									<input type="text" name="code1" class="form-control" placeholder="-Product Code-">
   							</div>
                 <div class="form-group col-md-6">
-                    <label for="inputemail">Repeat Model</label>
-                    <input type="text" name="email" id="inputmodel2" class="form-control" placeholder="-Repeat Product Code-">
+                    <label for="inputemail">Repeat Product Model</label>
+                    <input type="text" name="code2" class="form-control" placeholder="-Repeat Product Code-">
                 </div>
               </div>  
   							<div class="form-row" id="row2">
   								<div class="form-group col-md-6">
       								<label for="inputmfirstname">Product Type</label>
-      								<input type="text" class="form-control" id="inputmfirstname" placeholder="ex. Monitor, Keyboard, etc." name="mfname">
+      								<input type="text" class="form-control" placeholder="ex. Monitor, Keyboard, etc." name="producttype">
     							</div>
     							<div class="form-group col-md-6">
-      								<label for="inputmlname">Number of Stocks</label>
-      								<input type="text" class="form-control" id="inputmlname" placeholder="-Number of Product received-" name="mlname">
+      								<label for="inputmlname">Quantity</label>
+      								<input type="text" class="form-control"  placeholder="-Number of Product received-" name="stocks">
     							</div>
   							</div>
- 								<button type="submit" class="btn btn-lg btn-success" name="signup-btn">Add Item</button>
+ 								<button type="submit" class="btn btn-lg btn-success" name="additembtn">Add Item</button>
 						</form>
 			</div>
 		</div>
