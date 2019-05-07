91c28ad4111fd6ff134c85fe26317fb83ad5b404
Gerald Anderson <garciaricopaulo30@gmail.com>
2019-05-07 10:21:07 +0800

    Update additem.php

--- additem.php
+++ additem.php
@@ -14,55 +14,40 @@ else{
 <body>
 <div class="container">
 			<div class="card-header text-center">
-				<a href="homepage.php" class="btn btn-danger float-left">Go Back</a><h2 style="color: white;">Add Item</h2>
+				<a href="homepage.php" class="btn btn-danger float-left">Go Back</a><h1 style="color: black; margin-right: 90px;">Add Item</h1>
 						<form id="form1" action="includes/addemployee.inc.php" method="POST">
-							<label for="row1"><h3> Tell me more about yourself! </h3></label>
-  							<div class="form-row" id="row1">
+							<label for="row1"><h3> Item Details </h3></label>
+  							<div class="form-row" id="row1" style="margin-top: 0px!important;">
     							<div class="form-group col-md-6">
-      								<label for="inputfirstname">First Name</label>
-      								<input type="text" class="form-control" id="inputfirstname" placeholder="Juan" name="fname">
+      								<label for="inputfirstname">Company</label>
+      								<input type="text" class="form-control" id="inputfirstname" placeholder="Anderson Group BPO Inc." name="companyname">
     							</div>
     							<div class="form-group col-md-6">
-     								 <label for="inputlastname">Last Name</label>
-     								 <input type="text" class="form-control" id="inputlastname" placeholder="dela Cruz" name="lname">
+     								 <label for="inputlastname">Brand</label>
+     								 <input type="text" class="form-control" id="inputlastname" placeholder="Acer" name="productbrand">
     							</div>
   							</div>
-  								<div class="form-group">
-  									<label for="inputemail"> Email </label>
-  									<input type="text" name="email" class="form-control" placeholder="someone@example.com">
+                <div class="form-row" id="row2">
+  								<div class="form-group col-md-6">
+  									<label for="inputemail">Model</label>
+  									<input type="text" name="email" id="inputmodel1" class="form-control" placeholder="-Product Code-">
   							</div>
-  							<label for="row2"><h3>Who's your Manager?</h3></label>
+                <div class="form-group col-md-6">
+                    <label for="inputemail">Repeat Model</label>
+                    <input type="text" name="email" id="inputmodel2" class="form-control" placeholder="-Repeat Product Code-">
+                </div>
+              </div>  
   							<div class="form-row" id="row2">
   								<div class="form-group col-md-6">
-      								<label for="inputmfirstname">Manager's First Name</label>
-      								<input type="text" class="form-control" id="inputmfirstname" placeholder="John Mark" name="mfname">
+      								<label for="inputmfirstname">Product Type</label>
+      								<input type="text" class="form-control" id="inputmfirstname" placeholder="ex. Monitor, Keyboard, etc." name="mfname">
     							</div>
     							<div class="form-group col-md-6">
-      								<label for="inputmlname">Last Name</label>
-      								<input type="text" class="form-control" id="inputmlname" placeholder="Causing" name="mlname">
+      								<label for="inputmlname">Number of Stocks</label>
+      								<input type="text" class="form-control" id="inputmlname" placeholder="-Number of Product received-" name="mlname">
     							</div>
   							</div>
-  							<label for="row3"><h3>Account Details</h3></label>
-  							<div class="form-row" id="row3">
-									<div class="form-group col-md-12">
-  										<label for="username"> Username </label>
-  										<input type="text" name="username" id="username" class="form-control" placeholder="rocketwarrior">
-									</div>
-									<div class="form-group col-md-6">
-  										<label for="password1"> Password </label>
-  										<input type="password" name="password1" id="password1" class="form-control">
-									</div>
-									<div class="form-group col-md-6">
-  										<label for="password2"> Repeat Password </label>
-  										<input type="password" name="password2" id="password2" class="form-control">
-									</div>
-  							</div>
-  							<label for="position">Sign Up as</label>
-  							<select class="form-control" name="position" value="staff">
-                <option>Staff</option>
-                </select> 								
-                <br>
- 								<button type="submit" class="btn btn-lg btn-success btn-block" name="signup-btn">Add Employee</button>
+ 								<button type="submit" class="btn btn-lg btn-success" name="signup-btn">Add Item</button>
 						</form>
 			</div>
 		</div>
