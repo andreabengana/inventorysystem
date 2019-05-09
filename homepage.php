f33d9e25833b7a0b72ae5acca20d9b1c0532c0ed
Andre Abengana <john.abengana@outlook.com>
2019-05-09 10:20:38 +0800

    created viewmonitor page, edited navbar

--- homepage.php
+++ homepage.php
@@ -14,9 +14,9 @@ else{
 	 	<div class="row">
 	 		<div class="col">
 	 			<div class="card" style="width: 18rem;">
-  					<img class="card-img-top" src="resources/monitor.jpg" alt="Card image cap">
+  					<a href="viewmonitordetail.php"><img class="card-img-top" src="resources/monitor.jpg" alt="Card image cap"></a>
   						<div class="card-body">
-   							 <p class="card-text">Total Number of Monitors: <?php echo $monitorcount?></p>
+   							<p class="card-text">Total Number of Monitors: <?php echo $monitorcount?></p>
   						</div>
 				</div>
 	 		</div>
@@ -24,7 +24,7 @@ else{
 	 			<div class="card" style="width: 18rem;">
   					<img class="card-img-top" src="resources/monitor.jpg" alt="Card image cap">
   						<div class="card-body">
-   							 <p class="card-text">Total Number of Monitors: <?php?></p>
+   							<p class="card-text">Total Number of Keyboard: <?php echo $kybcount?></p>
   						</div>
 				</div>
 	 		</div>
@@ -32,7 +32,7 @@ else{
 	 			<div class="card" style="width: 18rem;">
   					<img class="card-img-top" src="resources/monitor.jpg" alt="Card image cap">
   						<div class="card-body">
-   							 <p class="card-text">Total Number of Monitors: <?php?></p>
+   							<p class="card-text">Total Number of Monitors: <?php?></p>
   						</div>
 				</div>
 	 		</div>
