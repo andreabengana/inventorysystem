79f833d9f808600416f3213dfc5841a2359e0845
Andre Abengana <john.abengana@outlook.com>
2019-05-28 16:23:50 +0800

    Update return.inc.php

--- includes/return.inc.php
+++ includes/return.inc.php
@@ -4,12 +4,19 @@
 	$text = $_POST["text"];  
 	$column_name = $_POST["column_name"];  
 	$sql = "UPDATE tblproducts SET ".$column_name."='".$text."' WHERE productID= ".$id;  
-	if(mysqli_query($connect, $sql)){  
-		echo 'Data Updated';
-		header("Location: ../return.php");
-
-	} else{
-		echo 'Data Not Updated'.$id." ".$text." ".$column_name." ";
-		echo $sql; 
+	
+	//if incoming changes is set from "Returned" or "Damaged" to "Dispatched" or "Available"
+	//to avoid manipulation of data
+	if($text == "Dispatched" || $text == "Available"){
+		echo 'Data Not Updated';
+	} else {
+		if(mysqli_query($connect, $sql)){  
+			echo 'Data Updated';
+			header("Location: ../return.php");
+	
+		} else{
+			echo 'Data Not Updated'.$id." ".$text." ".$column_name." ";
+			echo $sql; 
+		}
 	}
 ?>
