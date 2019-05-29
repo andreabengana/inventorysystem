2ee18da9ea20df7046134b8ebf6b1534213b7ff8
Andre Abengana <john.abengana@outlook.com>
2019-05-29 13:17:02 +0800

    Update transfer.inc.php

--- includes/transfer.inc.php
+++ includes/transfer.inc.php
@@ -35,37 +35,15 @@ if(isset($_POST['dispatchbtn'])){
 		while($row = mysqli_fetch_array($result)){
 			$tblproduct_array[] = $row;
 		}
-
-		// $sql = "SELECT dispatchID FROM `tbldispatch` ORDER BY dispatchID DESC LIMIT 1";
-		// $result = mysqli_query($conn, $sql);
-		// $maxDispatchID = mysqli_fetch_assoc($result);
-
-		// $maxDispatchID["dispatchID"] += 1;
-
-		// echo $maxDispatchID["dispatchID"];
 		
 		for ($i=0; $i < $quantity; $i++) { 
 
 			$sql = "UPDATE tblproducts SET productStatus = 'Dispatched', employeeAssigned = '$newemployeecode', workstationAssigned = '$newdispatchtoworkstation' WHERE productID = ".$tblproduct_array[$i]["productID"];
 			$result = mysqli_query($conn, $sql);
-
-			
-
-			// $sql = "SELECT userID FROM tblusers WHERE userName = '$sessionName'";
-			// $result = mysqli_query($conn, $sql);
-			// $row = mysqli_fetch_assoc($result);
-
-			// $sql = "INSERT INTO tbldispatch (`dispatchID`, `userID`, `productID`, `dispatchToDepartment`) VALUES 
-			// (".$maxDispatchID["dispatchID"].", ".$row['userID'].", '".$tblproduct_array[$i]["productID"]."', '$department');";
-			// $result = mysqli_query($conn, $sql);
 			
 		}
 
 	}
-
-
-
-
 
 }
 else{
